f95c1fe6036803bbea6fd5292d81a489c0f123d4
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-02 01:53:36 -0400

    Added code that shows how the return keyword works

---
+++ 24-ReturnKeyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ReturnKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Return
+            // - returns data back to the place where a method is invoked
+
+            double x;
+            double y;
+            double result;
+
+            Console.Write("Enter in number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter in number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Multiply(x, y);
+
+            Console.Write("Result: " + result);
+
+            Console.ReadKey();
+        }
+
+        static double Multiply(double x, double y)
+        {
+            double z = x * y;
+            return z;
+        }
+    }
+}
