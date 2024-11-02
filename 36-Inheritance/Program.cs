6484c7506a33f0dcdbc7739ebd117580a9ce8def
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-02 18:30:42 -0400

    Added example code showing simple Inheritance in C#.

---
+++ 36-Inheritance/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inheritance
+            //      1 or more child classes receiving fields, methods, etc. from a common parent
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+
+            // With car, bicycle, and boat, they all share the same field "speed" and same method "go"
+            //      that they inherited from the parent Vehicle class
+            // But they have unique fields like wheels.
+
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            car.go();
+
+            Console.WriteLine(bicycle.speed);
+            Console.WriteLine(bicycle.wheels);
+            bicycle.go();
+
+            Console.WriteLine(boat.speed);
+            Console.WriteLine(boat.wheels);
+            boat.go();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving!");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+    }
+
+    class Bicycle : Vehicle
+    {
+        public int wheels = 2;
+    }
+
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+    }
+}
