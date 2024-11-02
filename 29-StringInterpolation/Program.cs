b21be5d4ffdca6edc4f8502d96d61d2e27a084ee
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-02 02:26:09 -0400

    Adds example code that shows how string interpolation works

---
+++ 29-StringInterpolation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String Interpolation
+            //      Allows us to insert variables into a String literal
+            //      Precede a string literal with $
+            //      {} are placeholders
+
+            String firstName = "John";
+            String lastName = "Doe";
+            int age = 21;
+
+            // Concatenation way:
+            Console.WriteLine("Hello " + firstName + " " + lastName + ".");
+            Console.WriteLine("You are " + age + " years old.");
+
+            // This is String Interpolation:
+            Console.WriteLine($"Hello {firstName} {lastName}.");
+            Console.WriteLine($"You are {age} years old");
+
+            // This one allocates space
+            Console.WriteLine($"You are {age, 10} years old");
+
+
+            Console.ReadKey();
+        }
+    }
+}
