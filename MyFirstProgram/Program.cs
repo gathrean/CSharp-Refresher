55ab5818c62afc4a3497b3081b3c53f0a4d6d4f4
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-01 22:45:29 -0400

    Add project files.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("Hypotenuse: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
