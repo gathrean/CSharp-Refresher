ac8d7e1972b16fca4fae5802e6a4b28821f3b8ab
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-02 18:38:43 -0400

    Added a simple example of an abstract class in C#

---
+++ 37-AbstractClasses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AbstractClassesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Abstract Classes
+            //      "abstract" is a keyword 
+            //      it is a modifier that indicates missing components or incomplete implementation.
+            //
+            //      you can't make an instance or instatiate an abstract class
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+            //Vehicle vehicle = new Vehicle();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving!");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+        int maxSpeed = 500;
+    }
+
+    class Bicycle : Vehicle
+    {
+        public int wheels = 2;
+        int maxSpeed = 50;
+    }
+
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+        int maxSpeed = 100;
+    }
+}
