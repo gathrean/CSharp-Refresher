a69e4e85752eedc205e2d4d88b30ed199e9028a4
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-01 23:50:52 -0400

    Added while loops example code

---
+++ 15-WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // While Loops
+            // - They repeat some code as long as some condition is true.
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            while (name == "")
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hello " + name + "!);
+
+            Console.ReadKey();
+        }
+    }
+}
