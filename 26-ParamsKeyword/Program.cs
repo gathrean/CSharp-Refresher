cecb81a2b08f4d8f39ea9ea13c5a66dc4fae1f2a
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-02 02:06:05 -0400

    Added code that shows how using param keyword might be better to use in most cases than to overload methods

---
+++ 26-ParamsKeyword/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace ParamsKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // params Keyword
+            //  - A method parameter that takes avariable number of arguments
+            //  - The parameter type must be a single - dimensional array
+
+            //  - params keyword allows us to have a single method accept a varying amount of arguments
+            // So method overloading isn't always necessary
+
+            double total = CheckOut(3.99, 5.75, 15, 1.00, 10.25);
+
+            Console.WriteLine(total);
+
+            Console.ReadKey();
+        }
+
+        static double CheckOut(params double[] prices)
+        {
+            double total = 0;
+
+            foreach(double price in prices)
+            {
+                total += price;
+            }
+
+            return total;
+        }
+    }
+}
