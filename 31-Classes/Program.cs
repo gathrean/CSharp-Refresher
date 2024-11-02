d38d741873329c7f03bfe6a259c9e8b28d511a4f
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-02 02:44:30 -0400

    Added code that shows an example of classes in C#

---
+++ 31-Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Class
+            //      A bundle of related code.
+            //      Can be used as a blueprint to create objects (OOP)
+
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+
+            Console.ReadKey();
+        }
+    }
+}
