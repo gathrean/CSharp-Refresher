827a2f6ecf363361c9778298d35264c9595bed24
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-02 01:38:07 -0400

    Added example code showing a foreach loop

---
+++ 22-ForEachLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForEachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach Loop
+            // - A simpler way to iterate over an array
+            // - but it's less flexible
+
+            String[] cars = { "BMW", "Mustang", "Corvette" };
+
+            // It's less flexible though
+            // With for loops, we can customize the iteration, like i++, i+=2, or i--, etc.
+            // foreach only goes forwards
+
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
