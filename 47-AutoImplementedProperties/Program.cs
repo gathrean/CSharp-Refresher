da760cafa9370df41dbb3f87ba14ed7a85eb1e82
Gathrean Dela Cruz <gathrean@icloud.com>
2024-11-03 03:03:57 -0500

    Added a simple example showing Auto-Implemented Properties, a quick way to set up getters and setters in C#

---
+++ 47-AutoImplementedProperties/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AutoImplementedProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Auto-Implemented Properties
+
+            //      = Shortcut when no additional logic is required in the property
+            //      = You do not have to define a field for a property,
+            //        you only have to write 'get' and/or 'set'; inside the proeprty
+
+            Car car = new Car("Porsche");
+
+            Console.WriteLine(car.Model);
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        public String Model { get; set; } // This works as the below example
+
+        // This is the long way of setting up getters and setters (if this is all you're doing)
+
+        //String model;
+
+        //public String Model
+        //{
+        //    get { return model;  }
+        //    set { model = value; }
+        //}
+
+        public Car(String model)
+        {
+            this.Model = model;
+        }
+    }
+}
